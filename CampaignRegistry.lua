c3a7e03fd3dea75d831b0c3d45d7967a9b3d67b1
vmcreative <vincentmartincreative@gmail.com>
2020-12-13 16:46:28 -0500

    Add Directory
    Add all files in campaign directory

---
+++ CampaignRegistry.lua
@@ -0,0 +1,10 @@
+{
+	["OptDDCL"] = "off",
+	["OptINIT"] = "group",
+	["OptTFAC"] = "on",
+	["setup"] = true,
+	["OptRING"] = "on",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,spell,class,feat,portals",
+	["OptTBOX"] = "on",
+	["OptHRDD"] = "",
+}
