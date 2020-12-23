755de84706ec298d732829df55f4d1df1cf233cb
vmcreative <vincentmartincreative@gmail.com>
2020-12-23 00:52:07 -0500

    Game Night - 12-19-20
    The Tarrasque

--- CampaignRegistry.lua
+++ CampaignRegistry.lua
@@ -3,8 +3,8 @@
 	["OptINIT"] = "group",
 	["OptTFAC"] = "on",
 	["setup"] = true,
+	["OptTBOX"] = "on",
 	["OptRING"] = "on",
 	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,spell,class,feat,portals",
-	["OptTBOX"] = "on",
 	["OptHRDD"] = "",
 }
